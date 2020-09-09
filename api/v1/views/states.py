6f51a9b76ea73e4239505dad93fe6bb7742ef403
bryano13 <bryano13@hotmail.com>
2020-09-09 12:39:38 -0500

    task 7, States. Not completed yet

---
+++ api/v1/views/states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""states module"""
+
+from api.v1.views import app_views
+from flask import jsonify, abort, make_response, request
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """Retreives the list of all the States"""
+    states = []
+    all_states = storage.all(State).values()
+    for state in all_states:
+        states.append(state.to_dict())
+    return jsonify(states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves a single State object by it's id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """Deletes a state object by it's id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Creates (post) a new state object"""
+    if not request.get_json():
+        
