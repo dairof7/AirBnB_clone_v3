5fa2355af27ec1b6ff685401ee7871d7058a99eb
bryano13 <bryano13@hotmail.com>
2020-09-09 11:02:04 -0500

    task 6, error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,11 @@
 """Starts a Flask web application"""
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
-from models.state import State
-from models.amenity import Amenity
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,6 +15,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close_session(self):
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
