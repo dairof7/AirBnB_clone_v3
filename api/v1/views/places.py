bca0e83c9536bdf9b18e44f0575ab0f9db236fef
dairof7 <dairof7@gmail.com>
2020-09-09 21:28:50 -0500

    delete make_response

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -80,7 +80,7 @@ def put_place(place_id):
     if place is None:
         abort(404)
     if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        return jsonify({'error': 'Not a JSON'}), 400
     for attr, val in request.get_json().items():
         setattr(place, attr, val)
     place.save()
